5a92081f4334621eb02dbadcd55df78ad1833ef5
ThreePalmTrees <Chrosamofficial@gmail.com>
2022-01-12 16:03:24 +0100

    fix typo in api url

--- src/api.js
+++ src/api.js
@@ -11,10 +11,10 @@ const fetchApi = async (url) => {
 export const fetchItems = async (params = "") => {
   // @todo: if time helps, accept an object for `params instead of string & use URLSearchParams.set
   return await fetchApi(
-    `https://market-husam.herokuapp.com//api/items${params && `?${params}`}`
+    `https://market-husam.herokuapp.com/api/items${params && `?${params}`}`
   );
 };
 
 export const fetchCompanies = async () => {
-  return await fetchApi("https://market-husam.herokuapp.com//api/companies");
+  return await fetchApi("https://market-husam.herokuapp.com/api/companies");
 };
